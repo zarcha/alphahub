1f23790c788c9e982a4af863113a5b929def2cb9
KATSU-dev <55807384+KATSU-dev@users.noreply.github.com>
2020-08-20 19:14:00 +0100

    Add files via upload

--- js/leaderboardscript.js
+++ js/leaderboardscript.js
@@ -1,24 +1,25 @@
 var numOfRows;
 var usersJSONText = '{"users":[' +
-'{"name":"Katsu","rank":"B" },' +
-'{"name":"RayJT","rank":"A" },' +
-'{"name":"Fatred","rank":"A" },' +
-'{"name":"Zarch","rank":"A" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
+'{"name":"Katsu","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"RayJT","rank":"A", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"Fatred","rank":"A", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"Zarch","rank":"A", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"KaseyJoes","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
 
-'{"name":"KaseyJoes","rank":"B" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
-'{"name":"KaseyJoes","rank":"B" },' +
+'{"name":"KaseyJoes","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"KaseyJoes","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"KaseyJoes","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"KaseyJoes","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"KaseyJoes","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
+'{"name":"KaseyJoes","rank":"B", "wins":"10", "loss":"10", "draw":"10" },' +
 
-'{"name":"Xanthos","rank":"B" }]}';
+'{"name":"Xanthos","rank":"B", "wins":"10", "loss":"10", "draw":"10" }]}';
 
 obj = JSON.parse(usersJSONText);
 numOfRows = obj.users.length;
+
+scoreEveryone(obj.users);
+populateFirstFour(obj.users);
 
 var one = document.getElementById("one"),
 	two = document.getElementById("two"),
@@ -31,14 +32,60 @@ var index = numOfRows;
 	three.style.zIndex = index--;
 	four.style.zIndex = index--;
 	
+var lastIndex;
+	
 for(i = 0; index > 0; index--) {
 	var grey = document.createElement('div');
 	grey.id = 'odd';
 	grey.className = "lbrow";
+	grey.className += " greyRow";
 	grey.style.zIndex = index;
-	grey.style.background = "linear-gradient(0deg, rgba(40,40,40,1) 0%, rgba(45,45,45,1) 100%)";
-
+	
+	//grey.style.background = "linear-gradient(356deg, rgba(39,39,39,1) 40%, rgba(40,40,40,1) 50%)";
+	grey.style.backgroundColor = "#292929";
+	grey.style.boxShadow = "0px 0.15vh 0.8vh 0px rgba(0,0,0,0.25)";
+	grey.innerHTML = "<br><br>wow";
+	
+	grey.onmouseover = function() {onHoverRowStyleChange(this)};
+	grey.onmouseout = function() {onReleaseRowStyleChange(this)};
+	
+	
 	document.getElementById("greys").appendChild(grey);
 	console.log("xx" +index);
 }
 	
+
+function populateFirstFour(arrayEveryone) {
+	if(arrayEveryone.length > 0) {
+		var rowOne = document.getElementById("one");
+		
+	}
+	if(arrayEveryone.length > 1) {
+		var rowTwo = document.getElementById("two");
+	}
+	if(arrayEveryone.length > 2) {
+		var rowThree = document.getElementById("three");
+	}
+	if(arrayEveryone.length > 3) {
+		var rowFour = document.getElementById("four");
+	}
+}	
+	
+	
+function scoreEveryone(arrayEveryone) {
+
+	
+}	
+
+function onHoverRowStyleChange(x) {
+	lastIndex = x.style.zIndex;
+	x.style.zIndex = 2000000;
+	
+	console.log(x.id);
+	return;
+}
+
+function onReleaseRowStyleChange(x) {
+	x.style.zIndex = lastIndex;
+	return;
+}
